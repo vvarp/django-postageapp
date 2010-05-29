0a2b74f9e2c96f22e9e01a5f8b64a66223588c93
Maciek Szczesniak <maciek@vvnet.eu>
2010-05-29 17:45:12 +0100

    fixed packaging

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 import os
 from setuptools import setup, find_packages
 
-README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')
+README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README')
 
 setup(
     name='django-postageapp',
-    version='0.1.1',
-    description='Postageapp.com email backend for use with Django 1.2+',
+    version='0.1.1a',
+    description='Postageapp.com email backend for Django 1.2+',
     long_description=open(README_PATH).read(),
     author='Maciek Szczesniak',
     author_email='maciek@id43.net',
     url='http://github.com/vvarp/django-postageapp',
-    packages=find_packages(),
-    package_data={'': ['README.rst',]},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -21,6 +19,10 @@ setup(
         'Operating System :: OS Independent',
         'Framework :: Django',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
     ],
+
+    packages=find_packages(),
+    package_data={'': ['README',]},
+    package_dir={'django_postageapp': 'django_postageapp',},
+    zip_safe=False,
 )
